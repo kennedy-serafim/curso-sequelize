e9f48393dd84a797454c9ff72aabd766c344bbdc
kennedy-serafim <serafimkennedy@gmail.com>
2020-07-02 03:23:01 +0200

    User address migrate

--- src/database/migrations/20200702011035-create-user-addresses.js
+++ src/database/migrations/20200702011035-create-user-addresses.js
@@ -2,20 +2,53 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return await queryInterface.createTable('addresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+
+      zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return await queryInterface.dropTable('addresses');
+
   }
 };
